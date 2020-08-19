61dad1ba02f3643507419f6c43b693befdc96d46
offniklaus <67662045+offniklaus@users.noreply.github.com>
2020-08-19 09:49:27 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const Discord = require("discord.js")
 
 module.exports = bot => { 
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity("DashCraft", {type: ""});
+    bot.user.setActivity("OFFlimits | !help", {type: ""});
 }
